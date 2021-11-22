0653c4194b4d45073a698cb2faaa735a993ee6af
Mario <lombardomario01@gmail.com>
2021-11-22 14:40:42 +0100

    Bonus completed and refactoring

--- js/script.js
+++ js/script.js
@@ -13,12 +13,16 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 /*
 
 --DONE  1. Creo l'istanza di Vue => new Vue()
---DONE  2. Creo una proprietà a cui gli assegno valore null => x: null
+--DONE  2. Creo una proprietà a cui gli assegno un array vuoto => x: []
 --DONE  3 Creo un ciclo for che verrà eseguito 10 volte all'interno di mounted() => for (let i = 0; i < 10; i++)
     --DONE  3.1 Creo una variabile all'interno di mounted(), a cui assegno il valore di this => const self = this;
     --DONE  3.2 All'interno del ciclo creo la richiesta che dovrà effettuare => axios .get .then
         --DONE  3.2.1 Utilizzo l'endpoint apposito per generare l'email nel .get => https://flynn.boolean.careers/exercises/api/random/mail
-        --DONE  3.2.2 All'interno di .then assegno alla proprietà che avevo creato inizialmente il valore che mi restituirà la richiesta effettuata => self.email = argomento.x
+        --DONE  3.2.2 All'interno di .then pusho all'interno dell'array vuoto che avevo creato inizialmente il valore che mi restituirà la richiesta effettuata => self.emails.push(argomento.data.x)
+
+BONUS 
+
+--DONE  1. Inserisco un v-if nel tag "<ul>" in modo che le stampi tutte insieme => v-if="emails.length === 10"
 
 */
 
